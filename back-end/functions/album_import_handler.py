c06788f987a7b8f6ee8820b0f1ac7f61f3a003cf
Matthew Ruston <artanis0@gmail.com>
2026-02-08 16:10:17 -0500

    bug fix in album import handler

--- back-end/functions/album_import_handler.py
+++ back-end/functions/album_import_handler.py
@@ -10,7 +10,7 @@ def handler(event, context):
     for r in event.get('Records', []):
         try:
             payload = json.loads(r['body'])
-            if 'id' not in payload and 'service' not in payload:
+            if 'id' not in payload or 'service' not in payload:
                 raise Exception(f"Invalid SQS payload received {payload}")
             AlbumImportQueue().process(payload['id'], payload['service'])
         except Exception as exc:
